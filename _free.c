7ef3c06449772ece4216017b2929b09342785591
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-09-20 01:56:16 -0700

    Name header file monty

--- _free.c
+++ _free.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "main.h"
+#include "monty.h"
 
 /**
  * _free - free's a list
--- _push.c
+++ _push.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include "monty.h"
 
 /**
  * _push - pushes an element to the stack
