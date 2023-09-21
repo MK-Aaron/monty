a3a7e3d894f42f122bde54022ecd87b7853bee30
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-09-21 05:14:19 -0700

    Completed task 1

---
+++ _pint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+
+/**
+ * _pint - prints the value at the
+ * top of the stack, followed by a new line
+ * @h: pointer to first node
+ * @cnt: line count
+ */
+
+void _pint(stack_t **h, unsigned int cnt)
+{
+	if (h == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty", cnt);
+		_free(*h);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*h)->n);
+}
